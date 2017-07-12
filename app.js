e43548ff6b626f8f4247a4c3f455e61eb66e4057
Dom Clemmer <thisisdom4@gmail.com>
2017-07-12 15:36:27 -0400

    add webhook for messenger & affirmation.today

--- app.js
+++ app.js
@@ -5,10 +5,25 @@ var express = require('express')
 
 // APP DEFINITIONS
 var app = express()
+var affirmationTodayString = 'hoOUtqfjwQ'
+var app1String = 'WYfhUsS0v3'
+var app2String = 'iJ6TQVbTXH'
+var app3String = 'mPNXmPE2C9'
+var app4String = 'JRslAGYMSX'
+var app5String = '247y7YA1FC'
+var app6String = 'dhMNG7liLC'
+var app7String = 'tbzQb4qYgT'
+var app8String = '37aD3CcZtU'
+var app9String = 'u9nltnhPj7'
 
 // ROUTES
 
 // affirmation.today webhook
+var num = 12399
+app.get('/' + affirmationTodayString, (req,res) => {
+  res.send('hello world')
+})
+
 app.get('/webhook', function(req, res) {
     if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'jai_jai_ganesha') {
         console.log("Validating webhook");
