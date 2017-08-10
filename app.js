a6f25694f9e73323880419d0e27705526e602968
Dom Clemmer <thisisdom4@gmail.com>
2017-08-10 14:33:02 -0400

    remove need for promise

--- app.js
+++ app.js
@@ -16,19 +16,34 @@ var mongoose = require('mongoose')
 mongoose.connect('mongodb://dom:Losangeleslakers47@ds123182.mlab.com:23182/chat-sass-frontend')
 var db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
-var affirmationSchema = mongoose.Schema({text: String})
+var affirmationSchema = mongoose.Schema({
+  text: String
+})
 var Affirmation = mongoose.model('Affirmation', affirmationSchema)
 
-var feedbackSchema = mongoose.Schema({text: String})
+var feedbackSchema = mongoose.Schema({
+  text: String
+})
 var Feedback = mongoose.model('Feedback', feedbackSchema)
 
-var memberSchema = mongoose.Schema({organization: String, fbID: Number, fullName: String, enrolled: Boolean, timezone: Number, photo: String})
+var memberSchema = mongoose.Schema({
+  organization: String,
+  fbID: Number,
+  fullName: String,
+  enrolled: Boolean,
+  timezone: Number,
+  photo: String
+})
 memberSchema.virtual('firstName').get(() => {
   return this.fullName.split(' ')[0]
 })
 var Member = mongoose.model('Member', memberSchema)
 
-var groupSchema = mongoose.Schema({groupName: String, groupMembers: Array, organization: String})
+var groupSchema = mongoose.Schema({
+  groupName: String,
+  groupMembers: Array,
+  organization: String
+})
 var Group = mongoose.model('Group', groupSchema)
 
 var userSchema = mongoose.Schema({
@@ -37,9 +52,10 @@ var userSchema = mongoose.Schema({
   facebook: {
     userID: Number,
     pageID: Number,
-    accessToken: String
-  },
-  webhook: Number
+    pageAccessToken: String,
+    userAccessToken: String,
+    refreshToken: String
+  }
 })
 
 var User = mongoose.model('User', userSchema)
@@ -57,175 +73,205 @@ var app8String = '37aD3CcZtU'
 var app9String = 'u9nltnhPj7'
 
 // MIDDLEWARE
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(bodyParser.json())
 
 // ROUTES
 app.get('/', function(req, res) {
-    if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'jai_jai_ganesha') {
-        console.log("Validating webhook");
-        res.status(200).send(req.query['hub.challenge'])
-    } else {
-        console.error("Failed validation. Make sure the validation tokens match.")
-        res.sendStatus(403)
-    }
+  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'jai_jai_ganesha') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge'])
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.")
+    res.sendStatus(403)
+  }
 })
 
-app.post('/', function(req, res) {
-    var data = req.body
-    // Make sure this is a page subscription
-    if (data.object === 'page') {
-        // Iterate over each entry - there may be multiple if batched
-        console.log('data.entry: ' + JSON.stringify(data.entry))
-        data.entry.forEach(function(entry) {
-            var pageID = entry.id
-            var timeOfEvent = entry.time
-            // Iterate over each messaging event
-            entry.messaging.forEach(function(event) {
-                if (event.message) {
-                    // console.log('event: ' + JSON.stringify(event))
-                } else if (event.postback) {
-                  if (event.postback.payload === 'GET_STARTED_PAYLOAD') {
-                    User.findOne({ 'facebook.pageID': event.recipient.id}, (err, user) => {
-                      Member.findOne({ fbID: event.sender.id }, (err, member) => {
-                        if (err) {
-                          console.error(err)
-                        }
-                        if (member === null) {
-                          request({
-                            uri: 'https://graph.facebook.com/v2.6/' + event.sender.id + '?access_token=' + user.facebook.accessToken,
-                            method: 'GET'
-                          }, function(error, response, body) {
-                            if (error) {
-                              return console.error('upload failed:', error)
-                            }
-                            var data = JSON.parse(body)
-                            // NEED TO FIND ORG NAME AND REPLACE BELOW
-                            var newMember = new Member({organization: user.organization, fbID: event.sender.id, fullName: data.first_name + ' ' + data.last_name, photo: data.profile_pic, timezone: data.timezone})
-                            newMember.save((err, member) => {
-                              if (err) return console.error(err)
-                            })
-                            sendTextMessage(event.sender.id, user.facebook.accessToken, 'Thanks for signing up. More content to come!')
-                          })
-                        }
-                        else {
-                          sendTextMessage(event.sender.id, user.facebook.accessToken, 'Welcome back!')
-                        }
-                      })
+app.post('/', (req, res) => {
+  var data = req.body
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+    // Iterate over each entry - there may be multiple if batched
+    console.log('data.entry: ' + JSON.stringify(data.entry))
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id
+      var timeOfEvent = entry.time
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          // console.log('event: ' + JSON.stringify(event))
+        } else if (event.postback) {
+          if (event.postback.payload === 'GET_STARTED_PAYLOAD') {
+            User.findOne({
+              'facebook.pageID': event.recipient.id
+            }, (err, user) => {
+              Member.findOne({
+                fbID: event.sender.id
+              }, (err, member) => {
+                if (err) {
+                  console.error(err)
+                }
+                if (member === null) {
+                  request({
+                    uri: 'https://graph.facebook.com/v2.6/' + event.sender.id + '?access_token=' + user.facebook.accessToken,
+                    method: 'GET'
+                  }, function(error, response, body) {
+                    if (error) {
+                      return console.error('upload failed:', error)
+                    }
+                    var data = JSON.parse(body)
+                    // NEED TO FIND ORG NAME AND REPLACE BELOW
+                    var newMember = new Member({
+                      organization: user.organization,
+                      fbID: event.sender.id,
+                      fullName: data.first_name + ' ' + data.last_name,
+                      photo: data.profile_pic,
+                      timezone: data.timezone
                     })
-                  } else {
-                    eventHandler(event)
-                  }
+                    newMember.save((err, member) => {
+                      if (err) return console.error(err)
+                    })
+                    sendTextMessage(event.sender.id, user.facebook.accessToken, 'Thanks for signing up. More content to come!')
+                  })
                 } else {
-                    console.log("Webhook received unknown event: ", data)
+                  sendTextMessage(event.sender.id, user.facebook.accessToken, 'Welcome back!')
                 }
+              })
             })
-        })
-        res.sendStatus(200)
-    } else {
-      User.findOne({ 'organization': req.body.organization }, (err, user) => {
-        if (err) {
-          console.log(err)
+          } else {
+            eventHandler(event)
+          }
+        } else {
+          console.log("Webhook received unknown event: ", data)
         }
-
-        var sendees = []
-
-        var getSendees = new Promise(function(resolve, reject) {
-          for (var i = 0; i < req.body.groupNames.length; i++) {
-            Group.findOne({ groupName: req.body.groupNames[i], organization: req.body.organization }, (err, group) => {
-              if (err) {
-                return console.error(err)
-              } else {
-                for (var i = 0; i < group.groupMembers.length; i++) {
-                  sendees.push(group.groupMembers[i])
-                }
-                resolve(sendees)
-              }
-            })
-          }
-        })
-
-        getSendees.then((sendees) => {
-          for (var i = 0; i < sendees.length; i++) {
-            switch (req.body.type) {
-              case 'text':
-              console.log('sendee: ' + sendees[i])
-              console.log('accessToken: ' + user.facebook.accessToken)
-              console.log('msg text: ' + req.body.assetManifest.text)
-              sendTextMessage(sendees[i], user.facebook.accessToken, req.body.assetManifest.text)
-              break
-              case 'image':
-              // sendImage(sendees[i], req.body.assetManifest.image)
-              break
-              case 'both':
-              // sendImage(sendees[i], req.body.assetManifest.image)
-              // sendTextMessage(sendees[i], req.body.assetManifest.text)
-              break
-              default:
-
-            }
-          }
-          res.sendStatus(200)
-        })
-
       })
-      console.log(req.body)
-    }
+    })
+    res.sendStatus(200)
+  } else {
+    User.findOne({
+      'organization': req.body.organization
+    }, (err, user) => {
+      if (err) {
+        console.log(err)
+      }
+
+      var sendees = []
+
+      var getSendees = new Promise(function(resolve, reject) {
+        for (var i = 0; i < req.body.groupNames.length; i++) {
+          Group.findOne({
+            groupName: req.body.groupNames[i],
+            organization: req.body.organization
+          }, (err, group) => {
+            if (err) {
+              return console.error(err)
+            } else {
+              console.log(group)
+              for (var i = 0; i < group.groupMembers.length; i++) {
+                sendees.push(group.groupMembers[i])
+              }
+              resolve(sendees)
+            }
+          })
+        }
+      })
+
+      getSendees.then((sendees) => {
+            for (var i = 0; i < sendees.length; i++) {
+              if (req.body.image) {
+                // console.log('sendee: ' + sendees[i])
+                // console.log('accessToken: ' + user.facebook.accessToken)
+                // sendImage(sendees[i], req.body.assetManifest.image)
+                console.log('sending image message...')
+              }
+
+              if (req.body.videoURL) {
+                console.log('sending video link...')
+                // }
+                // switch (req.body.type) {
+                //   case 'text':
+                //   break
+                //   case 'image':
+                //   // sendImage(sendees[i], req.body.assetManifest.image)
+                //   break
+                //   case 'both':
+                //   // sendImage(sendees[i], req.body.assetManifest.image)
+                //   // sendTextMessage(sendees[i], req.body.assetManifest.text)
+                //   break
+                //   default:
+                //
+                // }
+              }
+
+              if (req.body.text) {
+                console.log('sendee: ' + sendees[i])
+                console.log('accessToken: ' + user.facebook.pageAccessToken)
+                // console.log('msg text: ' + req.body.assetManifest.text)
+                sendTextMessage(sendees[i], user.facebook.pageAccessToken, req.body.text)
+                console.log('sending text message...')
+                }
+              }
+      })
+      res.sendStatus(200)
+    })
+  }
 })
 
 function sendTextMessage(recipientId, accessToken, textMsg) {
   var messageData = {
-      recipient: {
-          id: recipientId
-      },
-      message: {
-          text: textMsg
-      }
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: textMsg
+    }
   }
 
   callSendAPI(accessToken, messageData)
 }
 
 function callSendAPI(accessToken, messageData) {
-    request({
-        uri: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {
-            access_token: accessToken
-        },
-        method: 'POST',
-        json: messageData
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: accessToken
+    },
+    method: 'POST',
+    json: messageData
 
-    }, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var recipientId = body.recipient_id;
-            var messageId = body.message_id;
+  }, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
 
-            console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
-        } else {
-            console.error("Unable to send message.")
-            console.error('response: ' + JSON.stringify(response))
-            console.error('error: ' + JSON.stringify(error))
-        }
-    });
+      console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
+    } else {
+      console.error("Unable to send message.")
+      console.error('response: ' + JSON.stringify(response))
+      console.error('error: ' + JSON.stringify(error))
+    }
+  });
 }
 
 function sendImage(recipientId, url) {
   var rand = Math.floor((Math.random() * 23) + 1);
 
-    var messageData = {
-        recipient: {
-            "id": recipientId
-        },
-        message: {
-            "attachment": {
-              "type": "image",
-              "payload": {
-                "url": url
-              }
-            }
+  var messageData = {
+    recipient: {
+      "id": recipientId
+    },
+    message: {
+      "attachment": {
+        "type": "image",
+        "payload": {
+          "url": url
         }
+      }
     }
-    callSendAPI(messageData)
+  }
+  callSendAPI(messageData)
 }
 
 // function callSendAPI(messageData) {
@@ -254,8 +300,8 @@ function sendImage(recipientId, url) {
 // SERVER LISTENING
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-    console.log('Server running on port ' + port)
+  console.log('Server running on port ' + port)
 })
 app.on('error', function() {
-    console.log(error)
+  console.log(error)
 })
