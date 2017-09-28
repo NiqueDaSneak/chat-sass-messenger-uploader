7658cc3766acc3e0cb73f89aa94a9667f4c29148
Dom Clemmer <thisisdom4@gmail.com>
2017-09-28 16:24:52 -0400

    testing custom for Levels

--- router/nightmakers.js
+++ router/nightmakers.js
@@ -3,6 +3,21 @@ var router = express.Router()
 
 router.post('/', (req, res, next) => {
   console.log('sucessfully passed to diff router')
+  var data = req.body
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+    // Iterate over each entry - there may be multiple if batched
+    console.log('data.entry: ' + JSON.stringify(data.entry))
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id
+      var timeOfEvent = entry.time
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+        } else if (event.postback) {
+          
+        }
+
   res.sendStatus(200)
 })
 
