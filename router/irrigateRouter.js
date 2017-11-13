eb9a5da32a405a64c14d0320f4c7583b216375c2
Dom Clemmer <thisisdom4@gmail.com>
2017-11-12 20:45:23 -0500

    fixing event undefined error

--- router/irrigateRouter.js
+++ router/irrigateRouter.js
@@ -119,7 +119,7 @@ irrigateRouter.post('/', (req, res, next) => {
           //       })
           //     })
           //
-          // }
+          }
 
           eventPostbackHandler(event)
         } else {
