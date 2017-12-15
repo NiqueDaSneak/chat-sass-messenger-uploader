72cec204f1b7cec3dde3366dd8af2ef04470b446
Dom Clemmer <thisisdom4@gmail.com>
2017-12-14 20:33:59 -0600

    fix user not defined error

--- postbacks/jtvPostbacks.js
+++ postbacks/jtvPostbacks.js
@@ -83,78 +83,79 @@ module.exports = (event) => {
   }
 
   if (event.postback.payload === "SHOW_CATS") {
-
-    let messageData = {
-      "recipient":{
-        "id": event.sender.id
-      },
-      "message": {
-        "attachment": {
-          "type": "template",
-          "payload": {
-            "template_type": "list",
-            "top_element_style": "LARGE",
-            "elements": [
-              {
-                "title": "Classic T-Shirt Collection",
-                "subtitle": "See all our colors",
-                "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
-                "buttons": [
-                  {
-                    "title": "View",
-                    "type": "web_url",
-                    "url": "https://peterssendreceiveapp.ngrok.io/collection",
-                    "messenger_extensions": true,
-                    "webview_height_ratio": "tall",
-                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                  }
-                ]
-              },
-              {
-                "title": "Classic White T-Shirt",
-                "subtitle": "See all our colors",
-                "default_action": {
-                  "type": "web_url",
-                  "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
-                  "messenger_extensions": false,
-                  "webview_height_ratio": "tall"
-                }
-              },
-              {
-                "title": "Classic Blue T-Shirt",
-                "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-                "subtitle": "100% Cotton, 200% Comfortable",
-                "default_action": {
-                  "type": "web_url",
-                  "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
-                  "messenger_extensions": true,
-                  "webview_height_ratio": "tall",
-                  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+    getUser().then((user) => {
+      let messageData = {
+        "recipient":{
+          "id": event.sender.id
+        },
+        "message": {
+          "attachment": {
+            "type": "template",
+            "payload": {
+              "template_type": "list",
+              "top_element_style": "LARGE",
+              "elements": [
+                {
+                  "title": "Classic T-Shirt Collection",
+                  "subtitle": "See all our colors",
+                  "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+                  "buttons": [
+                    {
+                      "title": "View",
+                      "type": "web_url",
+                      "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                      "messenger_extensions": true,
+                      "webview_height_ratio": "tall",
+                      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                    }
+                  ]
                 },
-                "buttons": [
-                  {
-                    "title": "Shop Now",
+                {
+                  "title": "Classic White T-Shirt",
+                  "subtitle": "See all our colors",
+                  "default_action": {
                     "type": "web_url",
-                    "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                    "messenger_extensions": false,
+                    "webview_height_ratio": "tall"
+                  }
+                },
+                {
+                  "title": "Classic Blue T-Shirt",
+                  "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                  "subtitle": "100% Cotton, 200% Comfortable",
+                  "default_action": {
+                    "type": "web_url",
+                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
                     "messenger_extensions": true,
                     "webview_height_ratio": "tall",
                     "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                  }
-                ]
-              }
-            ],
-            "buttons": [
-              {
-                "title": "View More",
-                "type": "postback",
-                "payload": "payload"
-              }
-            ]
+                  },
+                  "buttons": [
+                    {
+                      "title": "Shop Now",
+                      "type": "web_url",
+                      "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                      "messenger_extensions": true,
+                      "webview_height_ratio": "tall",
+                      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                    }
+                  ]
+                }
+              ],
+              "buttons": [
+                {
+                  "title": "View More",
+                  "type": "postback",
+                  "payload": "payload"
+                }
+              ]
+            }
           }
         }
       }
-    }
-    callSendAPI(user.pageAccessToken, messageData)
+      callSendAPI(user.pageAccessToken, messageData)
+    })
     // send list template
       // 'search jewlery type'
         // postback === 'CAT_TYPE_JEWEL'
