871b69925b207f26547f80cfc06ef50c4bf795a2
Dom Clemmer <thisisdom4@gmail.com>
2017-12-17 12:28:13 -0500

    test cart prop

--- postbacks/jtvPostbacks.js
+++ postbacks/jtvPostbacks.js
@@ -142,6 +142,8 @@ module.exports = (event) => {
   }
 
   if (event.postback.payload === "DONE") {
+
+    console.log('cart: ' + db.users.find({id : event.sender.id}).cart)
     // use event.sender.id => get cart id => get cart array
     // send cart array as carosel
       // item
