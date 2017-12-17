5ce188c4d5501f36ca5d1fdcea81c3624026482b
Dom Clemmer <thisisdom4@gmail.com>
2017-12-17 15:46:15 -0500

    done shopping logic

--- postbacks/jtvPostbacks.js
+++ postbacks/jtvPostbacks.js
@@ -273,22 +273,22 @@ module.exports = (event) => {
     }
 
     getUser().then((user) => {
-      // let messageData = {
-      //   "recipient":{
-      //     "id": event.sender.id
-      //   },
-      //   "message":{
-      //     "attachment":{
-      //       "type":"template",
-      //       "payload":{
-      //         "template_type":"generic",
-      //         "sharable": true,
-      //         "elements": elements
-      //       }
-      //     }
-      //   }
-      // }
-      // callSendAPI(user.pageAccessToken, messageData)
+      let messageData = {
+        "recipient":{
+          "id": event.sender.id
+        },
+        "message":{
+          "attachment":{
+            "type":"template",
+            "payload":{
+              "template_type":"generic",
+              "sharable": true,
+              "elements": elements
+            }
+          }
+        }
+      }
+      callSendAPI(user.pageAccessToken, messageData)
 
       setTimeout(() => {
         let messageData = {
