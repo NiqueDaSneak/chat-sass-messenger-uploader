c91b26de06b9b2eb5af4c05d53b4a9247c346854
Dom Clemmer <thisisdom4@gmail.com>
2018-01-09 12:08:37 -0500

    add get started postback

--- postbacks/skiPostbacks.js
+++ postbacks/skiPostbacks.js
@@ -1,1 +1,83 @@
 'use strict'
+
+var db = require('diskdb')
+db = db.connect('data', [])
+
+var Message = require('../models/messageModel.js')
+var Group = require('../models/groupModel.js')
+var User = require('../models/userModel.js')
+var Member = require('../models/memberModel.js')
+
+var request = require('request')
+var moment = require('moment')
+
+module.exports = (event) => {
+
+  function getUser() {
+    return new Promise(function(resolve, reject) {
+      User.findOne({
+        'pageID': event.recipient.id
+      }, (err, user) => {
+        resolve(user)
+      })
+    })
+  }
+
+  function findMember(user) {
+    return new Promise(function(resolve, reject) {
+      Member.findOne({
+        fbID: event.sender.id
+      }, (err, member) => {
+        if (err) {
+          console.error(err)
+        }
+        if (member === null) {
+          request({
+            uri: 'https://graph.facebook.com/v2.6/' + event.sender.id + '?access_token=' + user.pageAccessToken,
+            method: 'GET'
+          }, function(error, response, body) {
+            if (error) {
+              return console.error('upload failed:', error)
+            }
+            var facebookProfileResponse = JSON.parse(body)
+
+            // NEED TO FIND ORG NAME AND REPLACE BELOW
+            var newMember = new Member({
+              organization: user.organization,
+              fbID: event.sender.id,
+              fullName: facebookProfileResponse.first_name + ' ' + facebookProfileResponse.last_name,
+              photo: facebookProfileResponse.profile_pic,
+              timezone: facebookProfileResponse.timezone,
+              createdDate: moment().format('MM-DD-YYYY')
+            })
+
+            if (facebookProfileResponse.gender) {
+              newMember.gender = facebookProfileResponse.gender
+            }
+
+            newMember.save((err, member) => {
+              if (err) return console.error(err)
+              sendTextMessage(event.sender.id, user.pageAccessToken, 'Hey there! Welcome to the See N Ski Messenger Experience!')
+              setTimeout(() => {
+                sendTextMessage(event.sender.id, user.pageAccessToken, "Instructions: Click 'Reserve Now' in the menu below to make a purchase.")
+              }, 8000)
+              resolve()
+            })
+          })
+        } else {
+          sendTextMessage(event.sender.id, user.pageAccessToken, "Welcome back!")
+          resolve()
+        }
+      })
+    })
+  }
+
+  if (event.postback.payload === "GET_STARTED_PAYLOAD") {
+
+    // ENROLLING MEMBERS INTO THE IRRIGATE APP
+    getUser().then((user) => {
+      findMember(user)
+    })
+  }
+
+}
