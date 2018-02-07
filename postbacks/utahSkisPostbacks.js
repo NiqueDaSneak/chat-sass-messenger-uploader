9e97eb5add860f91ee0dbc800b240055ff4c4045
Dom Clemmer <thisisdom4@gmail.com>
2018-02-07 12:58:16 -0500

    add postbacks to file

--- postbacks/utahSkisPostbacks.js
+++ postbacks/utahSkisPostbacks.js
@@ -57,12 +57,38 @@ module.exports = (event) => {
             newMember.save((err, member) => {
               if (err) return console.error(err)
               // SEND WELCOME MESSAGES, THEN SEND PROMPT TO REQUEST ENROLLMENT
-
-              // sendTextMessage(event.sender.id, user.pageAccessToken, 'Welcome to the UNC Shuford Program Messenger Experience.')
+              sendTextMessage(event.sender.id, user.pageAccessToken, 'Welcome to the UtahSkis.com Experience!')
+              setTimeout(() => {
+                sendTextMessage(event.sender.id, user.pageAccessToken, 'Here you will be able to browse and purchase skis, snowboards, and apparel! You can also gain access to exclusive content and deals curated just for you!')
+                setTimeout(() => {
+                  let messageData = {
+                    "recipient":{
+                      "id": event.sender.id
+                    },
+                    "message":{
+                      "text": "Interested in exclusive content and deals during the Winter Olympics?",
+                      "quick_replies":[
+                        {
+                          "content_type":"text",
+                          "title":"Not Interested",
+                          "payload":"ENROLL_NO"
+                        },
+                        {
+                          "content_type":"text",
+                          "title":"Sure",
+                          "payload":"ENROLL_YES"
+                        }
+                      ]
+                    }
+                  }
+                  callSendAPI(user.pageAccessToken, messageData)
+                }, 4500)
+              }, 1500)
             })
           })
+
         } else {
-          sendTextMessage(event.sender.id, user.pageAccessToken, "Welcome back!")
+          sendTextMessage(event.sender.id, user.pageAccessToken, "Welcome back")
           resolve()
         }
       })
@@ -79,6 +105,64 @@ module.exports = (event) => {
       if (event.postback.payload === 'SHOP') {
         getUser().then((user) => {
           // SEND INITIAL CATS => CATS_MENS, CATS_WOMENS, CATS_KIDS, SUMMER
+          getUser().then((user) => {
+            let messageData = {
+              "recipient":{
+                "id": event.sender.id
+              },
+              "message":{
+                "attachment":{
+                  "type":"template",
+                  "payload":{
+                    "template_type":"generic",
+                    "elements":[
+                      {
+                        "title":"Women's",
+                        "buttons":[
+                          {
+                            "type":"postback",
+                            "title":"Shop",
+                            "payload":"CATS_WOMENS"
+                          }
+                        ]
+                      },
+                      {
+                        "title":"Men's",
+                        "buttons":[
+                          {
+                            "type":"postback",
+                            "title":"Shop",
+                            "payload":"CATS_MENS"
+                          }
+                        ]
+                      },
+                      {
+                        "title":"Kids's",
+                        "buttons":[
+                          {
+                            "type":"postback",
+                            "title":"Shop",
+                            "payload":"CATS_KIDS"
+                          }
+                        ]
+                      },
+                      {
+                        "title":"Summer",
+                        "buttons":[
+                          {
+                            "type":"postback",
+                            "title":"Shop",
+                            "payload":"SUMMER"
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                }
+              }
+            }
+            callSendAPI(user.pageAccessToken, messageData)
+          })
         })
       }
 
@@ -87,6 +171,317 @@ module.exports = (event) => {
           var gender = event.postback.payload.split('_')[2]
           var category = event.postback.payload.split('_')[1]
           getUser().then((user) => {
+            if (category === 'SKI') {
+              var elements = [
+                {
+                  "title":"Skis",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_SKIS_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Demo Skis",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_DEMOSKIS_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Ski Boots",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_SKIBOOTS_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Ski Bindings",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_SKIBINDINGS_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Ski Poles",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_SKIPOLES_" + gender
+                    }
+                  ]
+                }
+              ]
+            }
+
+            if (category === 'SNOW') {
+              var elements = [
+                {
+                  "title":"Snowboards",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_SNOWBOARDS_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Demo Snowboards",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_DEMOSNOWBOARDS_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Snowboard Bindings",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_SNOWBOARDBINDINGS_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Snowboard Bindings",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_SNOWBOARDBOOTS_" + gender
+                    }
+                  ]
+                }
+              ]
+            }
+
+            if (category === 'OUTER') {
+              var elements = [
+                {
+                  "title":"Jackets",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_SKIJACKETS_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Ski Pants",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_SKIPANTS_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Bibs and Suspenders",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_BIBSANDSUSPENDERS_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Snowboard Bindings",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_SNOWBOARDBOOTS_" + gender
+                    }
+                  ]
+                }
+              ]
+            }
+
+            if (category === 'CLOTH') {
+              var elements = [
+                {
+                  "title":"Fleece",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_FLEECE_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Hoodies",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_HOODIES_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Sweaters",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_SWEATERS_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Turtlenecks",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_TURTLENECKS_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Thermals",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_THERMALS_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"T-Shirts",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_TSHIRTS_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Socks",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_SOCKS_" + gender
+                    }
+                  ]
+                }
+              ]
+            }
+
+            if (category === 'ACCESS') {
+              // GLOVES, GOGGLES, HELMETS, BAGS, AVALANCE, AUDIO
+              var elements = [
+                {
+                  "title":"Gloves",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_GLOVES_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Goggles",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_GOGGLES_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Helmets",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_HELMETS_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Bags",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_BAGS_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Thermals",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_AVALANCE_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"T-Shirts",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_TSHIRTS_" + gender
+                    }
+                  ]
+                },
+                {
+                  "title":"Socks",
+                  "buttons":[
+                    {
+                      "type":"postback",
+                      "title":"Shop",
+                      "payload":"SHOWPRODUCTS_SOCKS_" + gender
+                    }
+                  ]
+                }
+              ]
+            }
+
+            let messageData = {
+              "recipient":{
+                "id": event.sender.id
+              },
+              "message":{
+                "attachment":{
+                  "type":"template",
+                  "payload":{
+                    "template_type":"generic",
+                    "elements": elements
+                  }
+                }
+              }
+            }
+            callSendAPI(user.pageAccessToken, messageData)
           // if category === SKI, SNOW, OUTER, CLOTH, ACCESS
             // show specific lists
               // SKI
@@ -105,6 +500,72 @@ module.exports = (event) => {
         } else {
           var gender = event.postback.payload.split('_')[1]
           getUser().then((user) => {
+            let messageData = {
+              "recipient":{
+                "id": event.sender.id
+              },
+              "message":{
+                "attachment":{
+                  "type":"template",
+                  "payload":{
+                    "template_type":"generic",
+                    "elements":[
+                      {
+                        "title":"Ski Gear",
+                        "buttons":[
+                          {
+                            "type":"postback",
+                            "title":"Shop",
+                            "payload":"CATS_SKI_" + gender
+                          }
+                        ]
+                      },
+                      {
+                        "title":"Snowboard Gear",
+                        "buttons":[
+                          {
+                            "type":"postback",
+                            "title":"Shop",
+                            "payload":"CATS_SNOW_" + gender
+                          }
+                        ]
+                      },
+                      {
+                        "title":"Outerwear",
+                        "buttons":[
+                          {
+                            "type":"postback",
+                            "title":"Shop",
+                            "payload":"CATS_OUTER_" + gender
+                          }
+                        ]
+                      },
+                      {
+                        "title":"Clothing",
+                        "buttons":[
+                          {
+                            "type":"postback",
+                            "title":"Shop",
+                            "payload":"CATS_CLOTH_" + gender
+                          }
+                        ]
+                      },
+                      {
+                        "title":"Accessories",
+                        "buttons":[
+                          {
+                            "type":"postback",
+                            "title":"Shop",
+                            "payload":"CATS_ACCESS_" + gender
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                }
+              }
+            }
+            callSendAPI(user.pageAccessToken, messageData)
             // CATS_SKI_WOMENS, CATS_SNOW_MENS, CATS_OUTER_KIDS, CATS_CLOTH_MENS, CATS_ACCESS_MENS
           })
         }
@@ -112,6 +573,7 @@ module.exports = (event) => {
 
       if (event.postback.payload.split('_')[0] === 'SHOWPRODUCTS') {
         var category = event.postback.payload.split('_')[1]
+        var gender = event.postback.payload.split('_')[2]
           getUser().then((user) => {
             // find list of products in category
             // check length
@@ -119,6 +581,16 @@ module.exports = (event) => {
               // else paginate
           })
       }
+
+      if (event.postback.payload === 'CALL') {
+
+      }
+
+      if (event.postback.payload === 'FAQ') {
+
+      }
+
+
     }
 
 
@@ -127,13 +599,19 @@ module.exports = (event) => {
         if (event.message.quick_reply.payload.split[0] === 'ENROLL') {
           if (event.message.quick_reply.payload.split[1] === 'YES') {
             getUser().then((user) => {
-
+              // send affirmative gif
             })
           } else {
             getUser().then((user) => {
-
+              // send negative gif
             })
+            sendTextMessage(event.sender.id, user.pageAccessToken, 'Instructions: scroll/swipe through categories to browse products. Tap “Buy now” to purchase.')
+            setTimeout(() => {
+              sendTextMessage(event.sender.id, user.pageAccessToken, 'Just click “Shop Now” in the menu to start browsing!')
+            }, 4000)
           }
+
+
         }
 
 
