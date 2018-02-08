a55e4720231f15c94e43ec0f4cb8252158fb207b
Dom Clemmer <thisisdom4@gmail.com>
2018-02-08 15:24:58 -0500

    fix unisex for avalance

--- postbacks/utahSkisPostbacks.js
+++ postbacks/utahSkisPostbacks.js
@@ -2738,13 +2738,7 @@ module.exports = (event) => {
             if (category === 'AVALANCHE') {
               searchParams = 'avalanche gear'
 
-              if (gender === 'MENS') {
-                gender = 'male'
-              } else if (gender === 'WOMENS') {
-                gender = 'female'
-              } else {
-                gender = 'kids'
-              }
+              gender = 'unisex'
 
               csv({
                 noheader: true,
