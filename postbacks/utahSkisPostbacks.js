9a5285ba93982ababea7f54fc303bfc6429af565
Dom Clemmer <thisisdom4@gmail.com>
2018-02-14 15:17:38 -0500

    remove typo

--- postbacks/utahSkisPostbacks.js
+++ postbacks/utahSkisPostbacks.js
@@ -156,7 +156,7 @@ module.exports = (event) => {
                         ]
                       },
                       {
-                        "title":"Kids's",
+                        "title":"Kids",
                         "buttons":[
                           {
                             "type":"postback",
